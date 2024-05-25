16bce7f75de1dcd70f8703b406cb3ea3f928d0ae
TienLee41 <letiennn41@gmail.com>
2024-05-25 22:38:09 +0700

    update category and store

--- GreatKart/store/views.py
+++ GreatKart/store/views.py
@@ -1,9 +1,14 @@
+from orders.models import OrderProduct
 from django.contrib import messages
+from store.forms import ReviewForm
 from django.shortcuts import get_object_or_404, redirect, render
 from django.core.paginator import Paginator
 from django.db.models import Q
 
+from store.models import Product, ReviewRating
+from carts.models import Cart, CartItem
 from category.models import Category
+from carts.views import _cart_id
 
 
 def store(request, category_slug=None):
